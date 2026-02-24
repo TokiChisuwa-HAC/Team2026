9d8ad879839c61fed3b95fbfac4d4e7ed4f97361
Toki Chisuwa <g024g1060@g.neec.ac.jp>
2026-02-24 15:08:31 +0900

    add:個人用のゲームプレイシーン追加

---
+++ nageru/Src/Scene/GamePlayScene/HitoshiScene/HitoshiScene.cpp
@@ -0,0 +1,112 @@
+#define GS_ENABLE_MESH_SHADOW
+#include <GSstandard_shader.h>
+#include <GSeffect.h>
+
+#include "HitoshiScene.h"
+#include "Field/Field.h"
+#include "Render/Camera/CameraTPS/CameraTPS.h"
+#include "Render/Light/Light.h"
+#include "Assets.h"
+
+#include "Actor/Player/Player.h"
+#include "Actor/Enemy/EnemyBasic/EnemyBasic.h"
+#include "Actor/Enemy/EnemySpike/EnemySpike.h"
+
+// 開始
+void HitoshiScene::start() {
+    // 終了フラグを初期化
+    is_end_ = false;
+    // 次のシーン名を初期化
+    next_scene_name_ = "TitleScene";
+    // 視錐台カリングを有効にする
+    gsEnable(GS_FRUSTUM_CULLING);
+    // デフォルトシェーダーの初期化
+    gsInitDefaultShader();
+    // シャドウマップの作成
+    static const GSuint shadow_map_size[] = { 2048, 2048 };
+    gsCreateShadowMap(2, shadow_map_size, GS_TRUE);
+    // シャドウマップを適用する距離(視点からの距離）
+    gsSetShadowMapDistance(60.0f);
+    // カスケードシャドウマップの分割位置を調整（デフォルトは0.5）
+    gsSetShadowMapCascadeLamda(0.7f);
+    // シャドウの濃さを設定(0.0:濃い～1.0:薄い)
+    gsSetShadowMapAttenuation(0.0f);
+    // シャドウマップのポリゴンオフセットを設定する
+    gsEnableShadowMapPolygonOffset();
+    gsSetShadowMapPolygonOffset(2.5f, 1.0f);
+    // ライトマップの読み込み
+    gsLoadLightmap(0, "Assets/Lightmap/Lightmap.txt");
+    // リフレクションプローブの読み込み
+    gsLoadReflectionProbe(0, "Assets/RefProbe/ReflectionProbe.txt");
+
+    // スカイボックスメッシュの読み込み
+    gsLoadMeshFixed(Mesh_Skybox, "Assets/Skybox/skydome.msh");
+    // 描画用オクツリーの読み込み
+    gsLoadOctree(Octree_Stage, "Assets/Octree/stage.oct");
+    // 衝突判定用オクツリーの読み込み
+    gsLoadOctree(Octree_Collider, "Assets/Octree/stage_collider.oct");
+
+    // プレイヤーメッシュの読み込み
+    gsLoadSkinMesh(Mesh_Player, "Assets/Player/player.mshb");
+    // 基礎エネミーメッシュの読み込み
+    gsLoadSkinMesh(Mesh_EnemyBasic, "Assets/Enemy/EnemyBasic/enemy_basic.mshb");
+    // とげエネミーメッシュの読み込み
+    gsLoadSkinMesh(Mesh_EnemySpike, "Assets/Enemy/EnemySpike/enemy_spike.mshb");
+
+    // フィールドクラスの追加
+    world_.add_field(new Field{ Octree_Stage, Octree_Collider, Mesh_Skybox });
+    // カメラクラスの追加
+    world_.add_camera(new CameraTPS{
+        &world_, GSvector3{ 0.0f, 6.2f, -4.8f }, GSvector3{ 0.0f, 1.92, 0.0f } });
+    // ライトクラスの追加
+    world_.add_light(new Light{ &world_ });
+
+    // プレイヤーの追加
+    world_.add_actor(new Player{ &world_, GSvector3{ -30.0f, 0.125f, 0.0f } });
+    // 基礎エネミーの追加
+    world_.add_actor(new EnemyBasic{ &world_, GSvector3{ -30.0f, 0.125f, 10.0f } });
+    // とげエネミーの追加
+    world_.add_actor(new EnemySpike{ &world_, GSvector3{ -20.0f, 0.125f, 10.0f } });
+}
+
+// 更新
+void HitoshiScene::update(float delta_time) {
+    // ワールドの更新
+    world_.update(delta_time);
+}
+
+// 描画
+void HitoshiScene::draw() const {
+    // ワールドの描画
+    world_.draw();
+}
+
+// 終了しているか？
+bool HitoshiScene::is_end() const {
+    // 終了フラグを返す
+    return is_end_;
+}
+
+// 次のシーンを返す
+std::string HitoshiScene::next() const {
+    return next_scene_name_;
+}
+
+// 終了
+void HitoshiScene::end() {
+    // ワールドを消去
+    world_.clear();
+    // 再生中のエフェクトを停止する
+    gsStopAllEffects();
+    // メッシュの削除
+    gsDeleteMesh(Mesh_Skybox);
+    // スケルトンの削除
+
+    // アニメーションの削除
+
+    // オクトリーの削除
+    gsDeleteOctree(Octree_Stage);
+    gsDeleteOctree(Octree_Collider);
+    // エフェクトの削除
+
+}
--- nageru/Src/main.cpp
+++ nageru/Src/main.cpp
@@ -2,6 +2,10 @@
 #include "Scene/SceneManager.h"
 #include "Scene/TitleScene/TitleScene.h"
 #include "Scene/GamePlayScene/GamePlayScene.h"
+#include "Scene/GamePlayScene/HitoshiScene/HitoshiScene.h"
+#include "Scene/GamePlayScene/TokiScene/TokiScene.h"
+#include "Scene/GamePlayScene/WataruScene/WataruScene.h"
+#include "Scene/GamePlayScene/YoshinobuScene/YoshinobuScene.h"
 #include <GSeffect.h>
 
 // ゲームクラス
@@ -19,7 +23,11 @@ public:
 		scene_manager_.add("TitleScene", new TitleScene());
 		// ゲームプレイシーンの追加
 		scene_manager_.add("GamePlayScene", new GamePlayScene());
-		// ゲームプレイシーンから開始
+		scene_manager_.add("HitoshiScene", new HitoshiScene());
+		scene_manager_.add("TokiScene", new TokiScene());
+		scene_manager_.add("WataruScene", new WataruScene());
+		scene_manager_.add("YoshinobuScene", new YoshinobuScene());
+		// タイトルシーンから開始
 		scene_manager_.change("TitleScene");
 	}
 
